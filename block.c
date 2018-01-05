09d7f4bd02c01fc9468ea0a93d2cbcac5c1dbdd6
Dhruv Bansal <shrub.vandal@gmail.com>
2018-01-05 16:17:46 -0600

    Added docco for block

--- block.c
+++ block.c
@@ -12,68 +12,71 @@ bool block_eq(const struct block *b, const u8 *key)
   return memcmp(b->sha, key, sizeof(b->sha)) == 0;
 }
 
-bool add_block(struct block_map *block_map, struct block *b,
-		      struct block **genesis, char **block_fnames, size_t *num_misses)
+bool add_block(struct block_map *block_map,
+               struct block *b,
+               struct block **genesis,
+               char **block_fnames,
+               size_t *num_misses)
 {
-	struct block *prev, *old = block_map_get(block_map, b->sha);
+        struct block *prev, *old = block_map_get(block_map, b->sha);
 
-	if (old) {
-		warnx("Already have "SHA_FMT" from %s %lu/%u",
-		      SHA_VALS(b->sha),
-		      block_fnames[old->filenum],
-		      old->pos, old->bh.len);
-		block_map_delkey(block_map, b->sha);
-	}
-	
-	block_map_add(block_map, b);
-	if (is_zero(b->bh.prev_hash)) {//so hash of first bh is zero
-		*genesis = b;
-		b->height = 0;
-		*num_misses = 0;
-		return true;
-	}
+        if (old) {
+                warnx("Already have "SHA_FMT" from %s %lu/%u",
+                      SHA_VALS(b->sha),
+                      block_fnames[old->filenum],
+                      old->pos, old->bh.len);
+                block_map_delkey(block_map, b->sha);
+        }
+        
+        block_map_add(block_map, b);
+        if (is_zero(b->bh.prev_hash)) {
+                *genesis = b;
+                b->height = 0;
+                *num_misses = 0;
+                return true;
+        }
 
-	/* Optimistically search for previous: blocks usually in rough order */
-	prev = block_map_get(block_map, b->bh.prev_hash); 
-	if (prev) {
-		if (prev->height != -1) {
-			b->height = prev->height + 1; //if previous height is known, this bh is known
-			*num_misses = 0;
-			return true;
-		}
+        /* Optimistically search for previous: blocks usually in rough order */
+        prev = block_map_get(block_map, b->bh.prev_hash); 
+        if (prev) {
+                if (prev->height != -1) {
+                        b->height = prev->height + 1; //if previous height is known, this bh is known
+                        *num_misses = 0;
+                        return true;
+                }
 
-		/* Every 1000 blocks we didn't get height for, try recursing. */
-		if ((*num_misses)++ % 1000 == 0) { //iterate num_misses even if statement false
-			if (set_height(block_map, b)) {  //try to set block height if too many blocks have height -1 - important to note that if u miss height for one block u miss it for the following blocks (i think)
-				*num_misses = 0;
-				return true;
-			}
-		}
-	}
-	return false;
+                /* Every 1000 blocks we didn't get height for, try recursing. */
+                if ((*num_misses)++ % 1000 == 0) {
+                        if (set_height(block_map, b)) {
+                                *num_misses = 0;
+                                return true;
+                        }
+                }
+        }
+        return false;
 }
 
 bool set_height(struct block_map *block_map, struct block *b)
 {
-	struct block *i, *prev;
+        struct block *i, *prev;
 
-	if (b->height != -1)
-		return true;
+        if (b->height != -1)
+                return true;
 
-	i = b;
-	do {
-		prev = block_map_get(block_map, i->bh.prev_hash);
-		if (!prev)
-			return false;
-		prev->next = i;
-		i = prev;
-	} while (i->height == -1);
+        i = b;
+        do {
+                prev = block_map_get(block_map, i->bh.prev_hash);
+                if (!prev)
+                        return false;
+                prev->next = i;
+                i = prev;
+        } while (i->height == -1);
 
-	/* Now iterate forward, setting height for all. */
-	b->next = NULL;
-	for (i = prev->next; i; i = i->next) {
-		i->height = prev->height + 1;
-		prev = i;
-	}
-	return true;
+        /* Now iterate forward, setting height for all. */
+        b->next = NULL;
+        for (i = prev->next; i; i = i->next) {
+                i->height = prev->height + 1;
+                prev = i;
+        }
+        return true;
 }
--- block.h
+++ block.h
@@ -5,11 +5,69 @@
 #include "utils.h"
 #include "types.h"
 
-const u8 *keyof_block_map(const struct block *b);
-bool block_eq(const struct block *b, const u8 *key);
+/* Defines the block_map and the following methods:
+ *
+ *   block_map_init
+ *   block_map_clear
+ *   block_map_add
+ *   block_map_del
+ *   block_map_get
+ *   block_map_first
+ *   block_map_next
+ *
+ * See ccan/htable/htable_type.h for more details.
+ * 
+ */
 HTABLE_DEFINE_TYPE(struct block, keyof_block_map, hash_sha, block_eq, block_map);
 
+/* Return the SHA256 hash for the given block.
+ *
+ * This function is used to match blocks to keys in the block_map.
+ *
+ *  @param b -- pointer to a block struct
+ */
+const u8 *keyof_block_map(const struct block *b);
+
+/* Does the given block match the given key/hash?
+ *
+ *  @param b   -- pointer to a block struct
+ *  @param key -- pointer to a SHA256 block hash
+ */
+bool block_eq(const struct block *b, const u8 *key);
+
+/* Add the given block to the block map.
+ *
+ * If the block already exists in the block_map, it will be replaced
+ * by the given block (and a warning generated).
+ *
+ * If the block being added has a zero hash, it will be recognized as
+ * the genesis block.
+ *
+ * If a block with hash equal to the prev_hash of the block being
+ * added is found, the block being added will have its height set and
+ * the number of misses reset.  Otherwise, the number of misses will
+ * be incremented.
+ *
+ * If the number of misses reaches 1000, set_height is called in an
+ * attempt to connect known blocks.
+ *
+ *  @param block_map    -- pointer to the block map
+ *  @param b            -- pointer to a block struct
+ *  @param genesis      -- pointer to the genesis block struct
+ *  @param block_fnames -- pointer to an array of block filename strings
+ *  @param num_misses   -- pointer to the number of misses
+ *
+ */
 bool add_block(struct block_map *block_map, struct block *b, struct block **genesis, char **block_fnames, size_t *num_misses);
+
+/* Sets the heights of blocks in the block map if possible.
+ *
+ * Finds the last block with a known height then iterates forward
+ * setting the height of the rest of the blocks.
+ *
+ *  @param block_map -- pointer to the block map
+ *  @param b         -- pointer to a block struct
+ */
 bool set_height(struct block_map *block_map, struct block *b);
 
 #endif /* BITCOIN_ITERATE_BLOCK_H */
